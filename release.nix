c8bc536df0371bb8be682c8195e88659e37364e4
Sander van der Burg <svanderburg@gmail.com>
2013-04-04 15:09:23 +0000

    Minor fixes to release.nix

--- release.nix
+++ release.nix
@@ -17,22 +17,22 @@ let
         inherit officialRelease;
 
         buildInputs = [ socat pkgconfig libxml2 libxslt disnix dblatex tetex nukeReferences ];
-	
-	# Add documentation in the tarball
+        
+        # Add documentation in the tarball
         configureFlags = ''
-	  --with-docbook-rng=${docbook5}/xml/rng/docbook
-	  --with-docbook-xsl=${docbook5_xsl}/xml/xsl/docbook
-	'';
-	
-	preConfigure = ''
-	  # TeX needs a writable font cache.
+          --with-docbook-rng=${docbook5}/xml/rng/docbook
+          --with-docbook-xsl=${docbook5_xsl}/xml/xsl/docbook
+        '';
+        
+        preConfigure = ''
+          # TeX needs a writable font cache.
           export VARTEXFONTS=$TMPDIR/texfonts
-	'';
-	
-	preDist = ''
-	  make -C doc/manual install prefix=$out
-	  
-	  make -C doc/manual index.pdf prefix=$out
+        '';
+        
+        preDist = ''
+          make -C doc/manual install prefix=$out
+          
+          make -C doc/manual index.pdf prefix=$out
           cp doc/manual/index.pdf $out/index.pdf
 
           # The PDF containes filenames of included graphics (see
@@ -42,15 +42,15 @@ let
           # to Windows and Macs, so there should be no Linux binaries
           # in the closure).
           nuke-refs $out/index.pdf
-	  
-	  echo "doc-pdf manual $out/index.pdf" >> $out/nix-support/hydra-build-products
-	  echo "doc manual $out/share/doc/disnixos/manual" >> $out/nix-support/hydra-build-products
-	'';
+          
+          echo "doc-pdf manual $out/index.pdf" >> $out/nix-support/hydra-build-products
+          echo "doc manual $out/share/doc/disnixos/manual" >> $out/nix-support/hydra-build-products
+        '';
       };
 
     build =
       { tarball ? jobs.tarball {}
-      , system ? "x86_64-linux"
+      , system ? builtins.currentSystem
       , disnix ? (import ../../disnix/trunk/release.nix {}).build {}
       }:
 
@@ -61,6 +61,6 @@ let
         src = tarball;
 
         buildInputs = [ socat pkgconfig disnix ];
-      };      
+      };
   };
 in jobs
