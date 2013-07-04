7df338fc45148cf7555cb9163a2316fbccec0556
Sander van der Burg <svanderburg@gmail.com>
2013-07-04 18:44:04 +0200

    WIP test suite for infrastructure deployment

--- release.nix
+++ release.nix
@@ -61,5 +61,132 @@ let
         src = tarball;
         buildInputs = [ socat pkgconfig disnix ];
       };
+
+    tests = 
+      { nixos ? <nixos>
+      , disnix ? (import ../disnix/release.nix {}).build {}
+      , dysnomia ? (import ../dysnomia/release.nix {}).build {}
+      }:
+      
+      let
+        pkgs = import nixpkgs {};
+        
+        disnixos = build { system = builtins.currentSystem; };
+        
+        networkNix = pkgs.writeTextFile {
+          name = "network.nix";
+          
+          text = ''
+            {
+              testtarget1 = {pkgs, ...}:
+            
+              {
+                require = [
+                  "${nixos}/modules/virtualisation/qemu-vm.nix"
+                  "${nixos}/modules/testing/test-instrumentation.nix"
+                ];
+                #boot.loader.grub.device = "/dev/null";
+                #fileSystems."/".device = "/dev/null";
+                #services.openssh.enable = true;
+                services.disnix.infrastructure.hostname = "testtarget1";
+                services.nixosManual.enable = false;
+                boot.loader.grub.enable = false;
+                environment.systemPackages = [ pkgs.zip ];
+              };
+            
+              testtarget2 = {pkgs, ...}:
+              
+              {
+                require = [
+                  "${nixos}/modules/virtualisation/qemu-vm.nix"
+                  "${nixos}/modules/testing/test-instrumentation.nix"
+                ];
+                #boot.loader.grub.device = "/dev/null";
+                #fileSystems."/".device = "/dev/null";
+                #services.openssh.enable = true;
+                services.disnix.infrastructure.hostname = "testtarget1";
+                services.nixosManual.enable = false;
+                boot.loader.grub.enable = false;
+                environment.systemPackages = [ pkgs.hello ];
+              };
+            }
+          '';
+        };
+      
+        network = import networkNix;
+        
+        networkBuilds = map (targetName:
+          let
+            target = builtins.getAttr targetName network;
+          in
+          (import "${nixos}/lib/eval-config.nix" {
+            inherit nixpkgs;
+            modules = [ target ];
+          }).config.system.build.toplevel
+        ) (builtins.attrNames network);
+      
+        machine =
+          {config, pkgs, ...}:
+            
+          {
+            virtualisation.writableStore = true;
+            
+            ids.gids = { disnix = 200; };
+            users.extraGroups = [ { gid = 200; name = "disnix"; } ];
+            
+            services.dbus.enable = true;
+            services.dbus.packages = [ disnix ];
+            services.openssh.enable = true;
+            
+            jobs.disnix =
+              { description = "Disnix server";
+
+                wantedBy = [ "multi-user.target" ];
+                after = [ "dbus.service" ];
+                
+                path = [ pkgs.nix disnix ];
+
+                script =
+                  ''
+                    export HOME=/root
+                    disnix-service --dysnomia-modules-dir=${dysnomia}/libexec/dysnomia
+                  '';
+               };
+              
+            environment.systemPackages = [ pkgs.stdenv pkgs.nix disnix disnixos pkgs.hello pkgs.zip pkgs.busybox pkgs.module_init_tools ];
+          };
+      in
+      with import "${nixos}/lib/testing.nix" { system = builtins.currentSystem; };
+      
+      {
+        deployment = simpleTest {
+          nodes = {
+            coordinator = machine;
+            testtarget1 = machine;
+            testtarget2 = machine;
+          };
+          testScript = "";
+          /*testScript = 
+            ''
+              startAll;
+              
+              # Initialise ssh stuff by creating a key pair for communication
+              my $key=`${pkgs.openssh}/bin/ssh-keygen -t dsa -f key -N ""`;
+    
+              $testtarget1->mustSucceed("mkdir -m 700 /root/.ssh");
+              $testtarget1->copyFileFromHost("key.pub", "/root/.ssh/authorized_keys");
+
+              $testtarget2->mustSucceed("mkdir -m 700 /root/.ssh");
+              $testtarget2->copyFileFromHost("key.pub", "/root/.ssh/authorized_keys");
+              
+              $coordinator->mustSucceed("mkdir -m 700 /root/.ssh");
+              $coordinator->copyFileFromHost("key", "/root/.ssh/id_dsa");
+              $coordinator->mustSucceed("chmod 600 /root/.ssh/id_dsa");
+              
+              # Deploy the test NixOS network expression
+              $coordinator->mustSucceed("NIX_PATH=nixpkgs=${nixpkgs}:nixos=${nixos} SSH_OPTS='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no' disnixos-deploy-network ${networkNix} --show-trace >&2");
+            '';*/
+        };
+      };
   };
 in jobs
