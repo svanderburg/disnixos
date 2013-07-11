4d7046b2ce922d164c581e4dd2847582e8d702ca
Sander van der Burg <svanderburg@gmail.com>
2013-07-11 17:03:01 +0200

    Added some docs to the library Nix functions

--- data/lib.nix
+++ data/lib.nix
@@ -6,6 +6,17 @@ let
 in
 rec {
 
+  /*
+   * Takes a collection of NixOS network expressions and zips them into a list of
+   * NixOS modules.
+   *
+   * Parameters:
+   * networkFiles: A list of strings containing paths to NixOS network expressions
+   * nixOpsModel: Indicates whether the configuration is a NixOps model so that certain attributes are ignored.
+   *
+   * Returns:
+   * An attribute set in which the names refer to machine names and values to lists of NixOS modules
+   */
   generateMergedNetwork = networkFiles: nixOpsModel:
     let
       networks = map (networkFile: import networkFile) networkFiles;
@@ -13,6 +24,19 @@ rec {
     in
     if nixOpsModel then removeAttrs mergedNetwork [ "network" "resources" ] else mergedNetwork; # A NixOps model has a reserved network attributes that cannot be machines
   
+  /*
+   * Takes a merged network configuration and evaluates them producing a config
+   * attribute for each of them.
+   *
+   * Parameters:
+   * enableDisnix: Indicates whether Disnix must be enabled
+   * nixOpsModel: Indicates whether we should use NixOps specific settings
+   * useVMTesting: Indicates whether we should enable NixOS test instrumentation and VM settings
+   * useBackdoor: Indicates whether we should enable the backdoor
+   *
+   * Returns:
+   * An attribute set with evaluated machine configurationb properties
+   */
   generateConfigurations = network: enableDisnix: nixOpsModel: useVMTesting: useBackdoor:
     pkgs.lib.mapAttrs (targetName: configuration:
       evalConfig {
@@ -44,6 +68,16 @@ rec {
         extraArgs = { nodes = generateConfigurations network enableDisnix nixOpsModel useVMTesting useBackdoor; };
       }) network;
 
+  /*
+   * Generates a list of NixOS system profiles mapped to target machines.
+   *
+   * Parameters:
+   * configurations: An attribute set with evaluated configurations
+   * targetProperty: Attribute from the infrastructure model that is used to connect to the Disnix interface
+   *
+   * Returns:
+   * A list of attribute sets in which NixOS profiles are mapped to target machines
+   */
   generateProfiles = configurations: targetProperty:
     map (targetName:
       let
@@ -57,6 +91,16 @@ rec {
     ) (attrNames configurations)
   ;
   
+  /*
+   * Generates a list of activation items specifying on which machine to activate a NixOS configuration.
+   *
+   * Parameters:
+   * configurations: An attribute set with evaluated configurations
+   * targetProperty: Attribute from the infrastructure model that is used to connect to the Disnix interface
+   *
+   * Returns:
+   * A list of attribute sets representing activation items
+   */
   generateActivationMappings = configurations: targetProperty:
     map (targetName:
       let
@@ -73,6 +117,16 @@ rec {
     ) (attrNames configurations)
   ;
   
+  /*
+   * Generates a list of machines that are involved in the deployment process.
+   *
+   * Parameters:
+   * configurations: An attribute set with evaluated configurations
+   * targetProperty: Attribute from the infrastructure model that is used to connect to the Disnix interface
+   *
+   * Returns:
+   * A list of strings with connection attributes of each machine that is used
+   */
   generateTargetPropertyList = configurations: targetProperty:
     map (targetName:
       let
@@ -83,6 +137,21 @@ rec {
     ) (attrNames configurations)
   ;
 
+  /*
+   * Generates a manifest file consisting of a profile mapping and
+   * service activation mapping from the 3 Disnix models.
+   *
+   * Parameters:
+   * network: An evaluated network with machine configurations
+   * targetProperty: Attribute from the infrastructure model that is used to connect to the Disnix interface
+   * enableDisnix: Indicates whether Disnix must be enabled
+   * nixOpsModel: Indicates whether we should use NixOps specific settings
+   * useVMTesting: Indicates whether we should enable NixOS test instrumentation and VM settings
+   * useBackdoor: Indicates whether we should enable the backdoor
+   *
+   * Returns:
+   * An attributeset which should be exported to XML representing the manifest
+   */
   generateManifest = network: targetProperty: enableDisnix: nixOpsModel: useVMTesting: useBackdoor:
     let
       configurations = generateConfigurations network enableDisnix nixOpsModel useVMTesting useBackdoor;
@@ -91,7 +160,22 @@ rec {
       activation = generateActivationMappings configurations targetProperty;
       targets = generateTargetPropertyList configurations targetProperty;
     };
-
+  
+  /*
+   * Generates a distributed derivation file constisting of a mapping of store derivations
+   * to machines from the 3 Disnix models.
+   *
+   * Parameters:
+   * network: An evaluated network with machine configurations
+   * targetProperty: Attribute from the infrastructure model that is used to connect to the Disnix interface
+   * enableDisnix: Indicates whether Disnix must be enabled
+   * nixOpsModel: Indicates whether we should use NixOps specific settings
+   * useVMTesting: Indicates whether we should enable NixOS test instrumentation and VM settings
+   * useBackdoor: Indicates whether we should enable the backdoor
+   *
+   * Returns: 
+   * An attributeset which should be exported to XML representing the distributed derivation
+   */
   generateDistributedDerivation = network: targetProperty: enableDisnix: nixOpsModel: useVMTesting: useBackdoor:
     let
       configurations = generateConfigurations network enableDisnix nixOpsModel useVMTesting useBackdoor;
