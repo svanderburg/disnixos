4dad41c3e587ff0529265dac26b2a178b906cc8c
Sander van der Burg <svanderburg@gmail.com>
2018-08-03 21:53:25 +0200

    Make compatible with both master and 18.03 version of Nixpkgs

--- data/disnix-infrastructure-module.nix
+++ data/disnix-infrastructure-module.nix
@@ -10,37 +10,39 @@ in
         default = false;
         description = "Whether to enable infrastructure publishing";
       };
-    
+
       enableAuthentication = lib.mkOption {
         default = false;
         description = "Whether to publish authentication credentials through the infrastructure attribute (not recommended in combination with Avahi)";
       };
-    
+
       infrastructure = lib.mkOption {
         default = {};
         description = "An attribute set containing infrastructure model properties";
       };
-      
+
       properties = lib.mkOption {
         default = {};
         description = "An attribute set container arbitary machine properties";
       };
-      
+
       generateContainersExpr = lib.mkOption {
         description = "The path to the expression generating the container properties";
         type = lib.types.path;
       };
     };
   };
-  
+
   config = lib.mkIf cfg.enable {
     disnixInfrastructure.infrastructure = {
       properties = {
         hostname = config.networking.hostName;
       } // cfg.properties;
-      
-      system = if config.nixpkgs.localSystem.system == "" then builtins.currentSystem else config.nixpkgs.localSystem.system;
-      
+
+      system = if config.nixpkgs ? localSystem && config.nixpkgs.localSystem.system != "" then config.nixpkgs.localSystem.system # Support compatiblity with Nixpkgs 17.09 and newer versions
+        else if config.nixpkgs.system != "" then config.nixpkgs.system
+        else builtins.currentSystem;
+
       containers = lib.recursiveUpdate (import cfg.generateContainersExpr {
         inherit (cfg) enableAuthentication;
         inherit config lib;
