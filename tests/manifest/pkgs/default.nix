8341720282572b587c62d86b4ac26caab7bf31e7
Sander van der Burg <svanderburg@gmail.com>
2021-02-25 21:24:51 +0100

    Eliminate references to deprecated stdenv.lib

--- tests/manifest/pkgs/default.nix
+++ tests/manifest/pkgs/default.nix
@@ -4,12 +4,12 @@ rec {
   testService1 = import ./testService1.nix {
     inherit (pkgs) stdenv;
   };
-    
+
   testService2 = import ./testService2.nix {
-    inherit (pkgs) stdenv;
+    inherit (pkgs) stdenv lib;
   };
 
   testService3 = import ./testService3.nix {
-    inherit (pkgs) stdenv;
+    inherit (pkgs) stdenv lib;
   };
 }
